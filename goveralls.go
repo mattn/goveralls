e32ae1be0c091f75636ccf7f58938e1b2d706f45
Dustin Sallings <dustin@spy.net>
2013-12-01 15:42:23 -0800

    Separate gocov-specific handling from core reporting

--- goveralls.go
+++ goveralls.go
@@ -6,17 +6,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
@@ -68,45 +64,16 @@ type Response struct {
 	Error   bool   `json:"error"`
 }
 
-type GocovResult struct {
-	Packages []struct {
-		Name      string
-		Functions []struct {
-			Name       string
-			File       string
-			Start, End int
-			Statements []struct {
-				Start, End, Reached int
-			}
-		}
-	}
-}
-
-func runGocov() (io.ReadCloser, error) {
-	cmd := exec.Command("gocov")
-	args := []string{"gocov", "test"}
-	if *verbose {
-		args = append(args, "-v")
-	}
-	if *pkg != "" {
-		args = append(args, *pkg)
-	}
-	cmd.Args = args
-	cmd.Stderr = os.Stderr
-	ret, err := cmd.Output()
+func getCoverage() []*SourceFile {
+	r, err := loadGocov()
 	if err != nil {
-		return nil, err
+		log.Fatalf("Error loading gocov results: %v", err)
 	}
-
-	return ioutil.NopCloser(bytes.NewReader(ret)), nil
-}
-
-func loadCoverage() (io.ReadCloser, error) {
-	if *gocovjson == "" {
-		return runGocov()
-	} else {
-		return os.Open(*gocovjson)
+	rv, err := parseGocov(r)
+	if err != nil {
+		log.Fatalf("Error parsing gocov: %v", err)
 	}
+	return rv
 }
 
 func main() {
@@ -139,88 +106,13 @@ func main() {
 	//
 	// Initialize Job
 	//
-	var j Job
-	j.RunAt = time.Now()
-	j.RepoToken = flag.Arg(0)
-	j.ServiceJobId = uuid.New()
-	j.Git = collectGitInfo()
-	if *service != "" {
-		j.ServiceName = *service
-	}
-
-	cov, err := loadCoverage()
-	if err != nil {
-		log.Fatalf("Error getting coverage data: %v", err)
-	}
-
-	var result GocovResult
-	d := json.NewDecoder(cov)
-	err = d.Decode(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cov.Close()
-
-	sourceFileMap := map[string]*SourceFile{}
-	// Find all the files and load their content
-	fileContent := map[string][]byte{}
-	for _, pkg := range result.Packages {
-		for _, fun := range pkg.Functions {
-			b, ok := fileContent[fun.File]
-			if !ok {
-				b, err = ioutil.ReadFile(fun.File)
-				if err != nil {
-					log.Printf("Error reading %v: %v (skipping)", fun.File, err)
-					continue
-				}
-				fileContent[fun.File] = b
-				// Count the lines
-				sf := &SourceFile{
-					Name:     fun.File,
-					Source:   string(b),
-					Coverage: make([]interface{}, bytes.Count(b, []byte{'\n'})),
-				}
-				sourceFileMap[fun.File] = sf
-				j.SourceFiles = append(j.SourceFiles, sf)
-			}
-			sf := sourceFileMap[fun.File]
-
-			// First, mark all parts of a mentioned function as covered.
-			linenum := 0
-			for i := range b {
-				if i >= fun.End {
-					break
-				}
-				if b[i] == '\n' {
-					linenum++
-				}
-				if i >= fun.Start {
-					// Leaving off a newline at the end of
-					// the file can cause us to compute line
-					// numbers where there are not lines.
-					if linenum < len(sf.Coverage) {
-						sf.Coverage[linenum] = 1
-					}
-				}
-			}
-
-			// Then paint each statement as directed.  This will mark misses.
-			for _, st := range fun.Statements {
-				linenum := 0
-				for i := range b {
-					if i >= st.End {
-						break
-					}
-					if b[i] == '\n' {
-						linenum++
-					}
-					if i >= st.Start {
-						sf.Coverage[linenum] = st.Reached
-						break // only count the statement start
-					}
-				}
-			}
-		}
+	j := Job{
+		RunAt:        time.Now(),
+		RepoToken:    flag.Arg(0),
+		ServiceJobId: uuid.New(),
+		Git:          collectGitInfo(),
+		SourceFiles:  getCoverage(),
+		ServiceName:  *service,
 	}
 
 	b, err := json.Marshal(j)
