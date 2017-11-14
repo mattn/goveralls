b71a1e4855f87991aff01c2c833a75a07059c61c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-11-14 13:29:57 +0900

    fix test

--- tester/tester_test.go
+++ tester/tester_test.go
@@ -8,6 +8,6 @@ func TestSimple(t *testing.T) {
 	value := GoverallsTester()
 	expected := "hello world"
 	if value != expected {
-		t.Fatalf("Expected %v, but %d:", value, expected)
+		t.Fatalf("Expected %v, but %v:", value, expected)
 	}
 }
